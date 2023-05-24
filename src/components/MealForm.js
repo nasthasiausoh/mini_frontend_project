dbeb7405b9d6c5476fbd4b8af4db546312b39f8e
Nasthasia Usoh <nasthasia@hotmail.co.uk>
2023-05-24 17:04:29 +0100

    completed mini project

--- src/components/MealForm.js
+++ src/components/MealForm.js
@@ -3,68 +3,12 @@ import MealList from "./MealList";
 const MealForm = ({meal, fetchMealSearch}) => {
 
    
-
-    // let global_apiData;
-    // let mealsList = document.getElementById("mealsList");
-    // let filetMealForm = document.querySelector("#filterMealForm")
-
-    // async function getMealInfo() {
-    //     const apiData = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s");
-    //     const apiDataJSON = await apiData.json();
-    //     return apiDataJSON;
-    // }
-
-    // async function setUp() {
-    //     // console.log("Hi everyone");
-    //     global_apiData = await getMealInfo();
-
-    //     mealsList.innerHTML = "";
-
-    //     global_apiData.map(meal => {
-    //         createNewMealElement(meal);
-    //     })
-
-    // }
-    // setUp();
-
-    // function createNewMealElement(single_meal) {
-    //     const newListItem = document.createElement("li");
-    //     const mealName = single_meal.meal.strMeal;
-    //     newListItem.innerText = `${mealName}`
-    //     mealsList.appendChild(newListItem);
-    // }
-
-    // function handleFilterFormSubmit(event) {
-    //     event.preventDefault();
-    //     const inputValue = event.target[0].value;
-    //     filterMeals(inputValue)
-    // }
-
-    // filetMealForm.addEventListener("submit", handleFilterFormSubmit)
-
-
-    // function filterMeals(filterCriteria) {
-    //     const filteredMeals = global_apiData.filter(meal => {
-    //         const noCase = meal.meal.strMeal.toLowerCase();
-    //         return noCase.includes(filterCriteria.toLowerCase())
-    //     })
-
-    //     mealsList.innerHTML = "";
-
-    //     filteredMeals.map(meal => {
-    //         createNewMealElement(meal);
-    //     })
-    // }
-
-
-
-
     return ( 
         <> 
             <h2>Meal Options</h2>
             <form id = "filterMealForm">
-                
-                <hr/>
+            
+            <hr/>
                 <div className="search_bar">
                     <input id="search_input"
                         type="text"
@@ -74,10 +18,7 @@ const MealForm = ({meal, fetchMealSearch}) => {
                     {/* not sure about the above */}
                     <button type="submit">Enter</button>
                  </div>   
-                    
-                
 
-                
             </form>
         </>
      );
